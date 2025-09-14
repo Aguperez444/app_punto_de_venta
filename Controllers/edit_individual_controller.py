52db3fef074f32084ad525b11bbfb48046bab8cd
Agustín Perez <agustinperez444@gmail.com>
2025-09-14 19:25:14 -0300

    migración de la ventana add_stock completada (implica creación de nuevos controllers e incluso nueva ventana para el edit individual de stock de productos), nuevas funciones para repositorio y servicio de productos

---
+++ Controllers/edit_individual_controller.py
@@ -0,0 +1,27 @@
+from Views.individual_edit_window import VentanaEditIndividual
+from Services.ProductoService import ProductoService
+
+class EditIndividualController:
+    def __init__(self, invoqued_by_window, products_ids):
+        self.view = VentanaEditIndividual(invoqued_by_window, self)
+        self.producto_service = ProductoService()
+        self.products_ids = products_ids
+
+        self.view.render_view()
+
+
+    def get_products_to_edit(self):
+        productos = self.producto_service.get_products_by_id_list(self.products_ids)
+        self.view.pasar_al_cuadro(productos)
+
+
+    def confirmar_cambios(self, quantity: int):
+
+        if quantity < 0:
+            self.view.show_error('Error con la cantidad de stock, La cantidad a agregar solo puede '
+                                                                      'ser un numero entero positivo')
+
+        self.producto_service.add_stock_to_multiple_products(self.products_ids, quantity)
+
+        self.view.cambios_realizados()
+
